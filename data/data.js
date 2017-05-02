b12f3da00ee7d7ba3ab58a4a87ada5a1f2119c21
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-05-02 23:16:35 +0100

    cumulative flow

---
+++ data/data.js
@@ -0,0 +1,38 @@
+var cumulative_flow_data = {
+  "today":"May 03",
+  "May 01": {
+      "Schedule not received":216,
+      "Not received, scheduled":0,
+      "Received, not checked":0,
+      "Received, In progress":0,
+      "Ready for upload":0,
+      "Charts, tables, downloads configuration":0,
+      "Departmental sign off":0,
+      "Ready for publication":0,
+      "Published":0,
+      "Not being progressed for July":20
+  },
+  "May 02": {
+      "Schedule not received": 8,
+      "Not received, scheduled": 193,
+      "Received, not checked": 15,
+      "Received, In progress": 0,
+      "Ready for upload": 0,
+      "Charts, tables, downloads configuration": 0,
+      "Departmental sign off": 0,
+      "Ready for publication": 0,
+      "Published": 0,
+      "Not being progressed for July": 20
+  },
+  "series":["Schedule not received",
+      "Not received, scheduled",
+      "Received, not checked",
+      "Received, In progress",
+      "Ready for upload",
+      "Charts, tables, downloads configuration",
+      "Departmental sign off",
+      "Ready for publication",
+      "Published",
+      "Not being progressed for July"],
+    "x_axis":["May 01", "May 02", "May 03"]
+    };
--- js/trellochart.js
+++ js/trellochart.js
@@ -31,11 +31,49 @@ function drawTrelloActivityChart(categories, data) {
     });
 }
 
+function drawTrelloActivityFlow(categories, series) {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'area'
+        },
+        title: {
+            text: 'Cumulative Flow'
+        },
+        xAxis: {
+            categories: categories,
+            tickmarkPlacement: 'on',
+            title: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Measures'
+            }
+        },
+        tooltip: {
+            split: true,
+            valueSuffix: ' measures'
+        },
+        plotOptions: {
+            area: {
+                stacking: 'normal',
+                lineColor: '#666666',
+                lineWidth: 1,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: '#666666'
+                }
+            }
+        },
+        series: series
+    });
+}
 
 var boardId = '58e75ebe43df25c997b03055'
 
 var departments = {'[DWP':0,'[BEIS':1,'[DCMS':2,'[DEFRA':3, '[HO':4,
-                    '[MoJ':5, '[CO':6, '[DH':7, '[DfE':8, '[DCLG':9};
+                    '[MoJ':5, '[MOJ':5, '[CO':6, '[DH':7, '[DfE':8, '[DCLG':9};
 var departmentNames = ['DWP', 'BEIS', 'DCMS', 'DEFRA', 'Home Office', 'MoJ', 'Cabinet Office', 'Health', 'Education', 'DCLG'];
 var chart = null;
 
@@ -66,34 +104,101 @@ function buildActivityChart() {
     getDataFromApi(function(lists, cards) {
 
         var listDict = {};
-        var listNames = []
+        var listNames = [];
         for(l in lists) {
             listDict[lists[l].id] = l;
             listNames.push(lists[l].name);
         }
 
-
-        var data = emptySeries(lists.length);
-
-        for(c in cards) {
-            var card = cards[c];
-            var listNumber = listDict[card.idList];
-            var deptNumber = getDepartment(card);
-
-            if(!(deptNumber === undefined)) {
-                data[deptNumber].data[listNumber] += 1;
-            }
-        }
+        var currentActivities = getActivities(lists, listDict, cards);
 
         if(chart) {
-            chart.data = data;
+            chart.data = currentActivities;
         } else {
-            chart = drawTrelloActivityChart(listNames, data);
+            chart = drawTrelloActivityChart(listNames, currentActivities);
         }
     });
 }
 
+function buildActivityFlow() {
+    getDataFromApi(function(lists, cards) {
 
+        var flow_data = buildEmptyFlowData();
+        addCurrentCards(flow_data, cards, lists);
+        setExistingData(flow_data);
+
+        if(chart) {
+            chart.data = flow_data;
+        } else {
+            chart = drawTrelloActivityFlow(cumulative_flow_data.x_axis, flow_data);
+        }
+    });
+}
+
+function buildEmptyFlowData() {
+    var serieses = [];
+    for(s in cumulative_flow_data.series) {
+        var series_name = cumulative_flow_data.series[s];
+        var series_data = [];
+        for(i in cumulative_flow_data.x_axis) {
+            series_data.push(0);
+        }
+        serieses.push({name:series_name, data:series_data});
+    }
+    return serieses;
+}
+
+function addCurrentCards(flow_data, cards, lists) {
+    var time_index = cumulative_flow_data.x_axis.indexOf(cumulative_flow_data.today);
+
+    var listDict = {};
+    for(l in lists) {
+        listDict[lists[l].name] = lists[l].id;
+    }
+    console.log(listDict);
+
+    for(s in flow_data) {
+        var list_id = listDict[flow_data[s].name];
+        var count = 0;
+        for(c in cards) {
+            if(cards[c].idList === list_id) { count += 1; }
+        }
+        flow_data[s].data[time_index] = count;
+    }
+
+    console.log(lists);
+    console.log(cards);
+}
+
+function setExistingData(flow_data) {
+    for(i in cumulative_flow_data.x_axis) {
+        var month = cumulative_flow_data.x_axis[i];
+        var data_item = cumulative_flow_data[month];
+        if(data_item) { setExistingDataItem(flow_data, month, data_item);}
+    }
+}
+function setExistingDataItem(flow_data, month, data_item) {
+    var month_index = cumulative_flow_data.x_axis.indexOf(month);
+    for(s in flow_data) {
+        var series = flow_data[s].name;
+        var value = data_item[series];
+        flow_data[s].data[month_index] = value;
+    }
+}
+
+function getActivities(lists, listDict, cards) {
+    var activities = emptySeries(lists.length);
+    for(c in cards) {
+        var card = cards[c];
+        var listNumber = listDict[card.idList];
+        var deptNumber = getDepartment(card);
+
+        if(!(deptNumber === undefined)) {
+            activities[deptNumber].data[listNumber] += 1;
+        }
+    }
+    return activities;
+}
 
 function getDataFromApi(success) {
     Trello.get("boards/" + boardId + '/lists', function(lists) {
@@ -116,5 +221,15 @@ function setupTrelloActivityChart(settings) {
         }, function failure() {
             // On failure go again
             setupTrelloActivityChart(settings);
+    });
+}
+
+function setupTrelloActivityFlowGraph(settings) {
+    authorise(function success() {
+        // On successful auth draw the chart
+        buildActivityFlow();
+    }, function failure() {
+        // On failure go again
+        setupTrelloActivityFlowGraph(settings);
     });
 }
